0a4c164b5f299ba2570b6aeccb5f8d56c861d7a8
André Monteiro <monteiro.hat@gmail.com>
2023-10-27 16:03:19 +0200

    adicionado o consumo da API

--- app.py
+++ app.py
@@ -1,10 +1,74 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify,request
+import requests
 
 app = Flask(__name__)
+
+API_BASE_URL = 'https://usvlbrk01rn001.azurewebsites.net' 
+CLIENT_ID = 'falcon_financiamentos'
+CLIENT_SECRET = 'F@lc0NF1n@nc1@m3nt0s'
+GRANT_TYPE = 'client_credentials'  # Apenas um exemplo, ajuste conforme necessário
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def get_token():
+    token_response = requests.post(
+        f"{API_BASE_URL}/token_endpoint",
+        data={
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': GRANT_TYPE
+        }
+    )
+    return token_response.json().get("access_token")
+
+
+@app.route('/simular', methods=['POST'])
+def get_dados():
+   # Pegar dados do POST
+    data = request.json
+
+    #Valida os daos de entrada
+    
+
+    # Autenticar para obter o token
+    token = get_token()
+    if not token:
+        return jsonify({"error": "Failed to get access token."}), 401
+
+    headers = {
+        'Authorization': f"Bearer {token}"
+    }
+
+    # Construir dados completos para simulação
+    simulation_data = {
+        "Nome": data.get("Nome"),
+        "CPF": data.get("CPF"),
+        "Email": data.get("Email"),
+        "Celular": data.get("Celular"),
+        "LocalizacaoImovel": data.get("LocalizacaoImovel"),
+        "EscolheuImovel": True,  # Padrão
+        "FinanciarDespesas": False,  # Padrão
+        "OutroParticipante": False,  # Padrão
+        "TipoImovel": data.get("TipoImovel"),
+        "ValorImovel": data.get("ValorImovel"),
+        "ValorEntrada": data.get("ValorEntrada"),
+        "PrazoMeses": data.get("PrazoMeses"),
+        "Nascimento": "1979-05-23",  # Padrão para este exemplo
+        "RendaMensal": 100000,  # Padrão para este exemplo
+        "SistemaAmortizador": 1,  # Padrão
+        "Seguradora": "ITAU",  # Padrão
+        "SeguradoraSantander": "ZURICH SANTANDER"  # Padrão
+    }
+
+
+    response = requests.post(f"{API_BASE_URL}/api/simulacao", headers=headers, json=simulation_data)
+
+    return response.json()
+   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/site.js
+++ static/js/site.js
@@ -22,6 +22,11 @@ function converterStringParaNumero(valorString) {
     return parseFloat(valorFormatado);
 }
 
+function removerCaracteresEspeciais(texto) {
+    return texto.replace(/[^0-9]/g, ''); // Remove tudo que não for dígito
+}
+
+
 //RANGE DE PERCENTUAL
 inputRange.addEventListener('input', function () {
     if (inputValorImovel.value) {
@@ -36,44 +41,44 @@ inputRange.addEventListener('input', function () {
 });
 
 //VALOR DO IMOVEL
-inputValorImovel.addEventListener('input', function() {
+inputValorImovel.addEventListener('input', function () {
     let valorImovel = converterStringParaNumero(inputValorImovel.value);
     let valorEntrada = converterStringParaNumero(inputValorEntrada.value);
 
     // Se o valor da entrada for maior que o valor do imóvel, ajuste-o
     if (valorEntrada > 0) {
-        
+
         //atualizarPercentual();
         calcular_range();
     }
-    
+
 });
-inputValorImovel.addEventListener('blur', function() {
+inputValorImovel.addEventListener('blur', function () {
     atualizaTotalFinanciamento();
-    
+
 });
 
 
 //VALOR DE ENTRADA
-inputValorEntrada.addEventListener('blur', function() {
+inputValorEntrada.addEventListener('blur', function () {
     let valorImovel = converterStringParaNumero(inputValorImovel.value);
     let valorEntrada = converterStringParaNumero(inputValorEntrada.value);
 
     // Se o valor da entrada for maior que o valor do imóvel, ajuste-o
     if (valorEntrada > valorImovel) {
         //inputValorEntrada.value = inputValorImovel.value; // Defina o valor da entrada igual ao valor do imóvel
-        $('#form-valor-entrada').val(inputValorImovel.value).mask('#.##0,00', {reverse: true}).trigger('input');
+        $('#form-valor-entrada').val(inputValorImovel.value).mask('#.##0,00', { reverse: true }).trigger('input');
     }
 
     // Atualizar o percentual após qualquer mudança no valor da entrada
     atualizarPercentual();
-    
+
 });
 
 //IDADE
-idadeInput.addEventListener('blur', function() {
+idadeInput.addEventListener('blur', function () {
     let valor = parseInt(idadeInput.value, 10);
-    
+
     if (valor < 18 || valor > 80) {
         alert('Por favor, insira uma idade entre 18 e 80 anos.');
         idadeInput.value = ''; // Limpa o valor se estiver fora do intervalo
@@ -91,7 +96,7 @@ function calcular_range() {
         maximumFractionDigits: 2
     });
 
-    $('#form-valor-entrada').val(valorFormatadoBrasileiro).mask('#.##0,00', {reverse: true}).trigger('input');
+    $('#form-valor-entrada').val(valorFormatadoBrasileiro).mask('#.##0,00', { reverse: true }).trigger('input');
     atualizaTotalFinanciamento();
 
 }
@@ -100,7 +105,7 @@ function calcular_range() {
 function atualizarPercentual() {
     let valorImovel = converterStringParaNumero(inputValorImovel.value);
     let valorEntrada = converterStringParaNumero(inputValorEntrada.value);
-    
+
     // Cálculo do percentual
     let percentual = (valorEntrada / valorImovel) * 100;
 
@@ -113,7 +118,7 @@ function atualizarPercentual() {
 function atualizaTotalFinanciamento() {
     let valorImovel = converterStringParaNumero(inputValorImovel.value);
     let valorEntrada = converterStringParaNumero(inputValorEntrada.value);
-    
+
     let totalFinanciar = valorImovel - valorEntrada;
 
     // Convertendo o valor para o formato brasileiro
@@ -123,4 +128,107 @@ function atualizaTotalFinanciamento() {
     });
 
     labelTotalFinanciar.textContent = "FINANCIAR: R$ " + totalFormatadoBrasileiro;
+}
+
+
+function simular() {
+    event.preventDefault();
+
+    //Valida os campos obrigatorios
+    const inputNome = document.getElementById('form-nome');
+    const inputEmail = document.getElementById('form-email');
+    const inputCpf = document.getElementById('form-cpf');
+    const inputCelular = document.getElementById('form-celular');
+    const inputIdade = document.getElementById('form-idade');
+    const selectEstado = document.getElementById('form-estado');
+
+    const cpfLimpo = removerCaracteresEspeciais(inputCpf.value);
+    const celularLimpo = removerCaracteresEspeciais(inputCelular.value);
+
+    //Nome
+    if (inputNome.value.trim() === "") {
+        inputNome.focus();
+        return;
+    }
+
+    //Email
+    if (inputEmail.value.trim() === "") {
+        inputEmail.focus();
+        return;
+    }
+
+    //Cpf
+    if (cpfLimpo.trim() === "") {
+        inputCpf.focus();
+        return;
+    }
+
+    //Celular
+    if (celularLimpo.trim() === "") {
+        inputCelular.focus();
+        return;
+    }
+
+     //Idade
+     if (inputIdade.value.trim() === "") {
+        inputIdade.focus();
+        return;
+    }
+
+    //Estado
+    if (!isEstadoSelecionado()) {
+        selectEstado.focus();
+        console.log("Por favor, selecione um estado.");
+        return;
+    }
+
+
+    if (!validaCPF(cpfLimpo)) {
+        inputCpf.focus();
+        console.log("CPF válido");
+        alert('Ops! Parece que o CPF informado não é válido.');
+        return;
+    }
+
+    //Mostra o preloader
+    document.querySelector(".preloader").style.display = "flex";
+
+    //Oculta os dados cadastrais
+    $('#dados-cadastrais').hide();
+
+}
+
+function isEstadoSelecionado() {
+    const selectEstado = document.getElementById('form-estado');
+    // Verificar pelo índice
+    if (selectEstado.selectedIndex === 0) {
+        return false;
+    }
+    return true;
+}
+
+
+function validaCPF(cpf) {
+    cpf = cpf.replace(/[^0-9]/g, ''); // Remove caracteres não numéricos
+
+    if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    for (let j = 9; j <= 10; j++) {
+        let soma = 0;
+        for (let i = 0; i < j; i++) {
+            soma += cpf[i] * ((j + 1) - i);
+        }
+        let resto = soma % 11;
+        if (resto < 2) {
+            resto = 0;
+        } else {
+            resto = 11 - resto;
+        }
+        if (resto !== parseInt(cpf.charAt(j))) {
+            return false;
+        }
+    }
+    return true;
 }
